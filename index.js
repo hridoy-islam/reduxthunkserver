fe866efaea639b85739e60b510cb31b76963a353
Hridoy Islam <hridoy4t@gmail.com>
2023-02-06 00:37:14 +0600

    delete done

--- index.js
+++ index.js
@@ -20,18 +20,47 @@ const client = new MongoClient(uri, {
 const run = async () => {
   try {
 
-    // const db = client.db("reduxthunk");
-    // const postCollection = db.collection("post");
+    const db = client.db("reduxthunk");
+    const postCollection = db.collection("post");
 
-    // app.get("/products", async (req, res) => {
-    //   const cursor = productCollection.find({});
-    //   const product = await cursor.toArray();
+    app.get('/posts', async (req, res)=> {
+      const cursor = await postCollection.find({});
+      const posts = await cursor.toArray();
+      res.send(posts);
+    })
 
-    //   res.send({ status: true, data: product });
-    // });
+    app.get('/post/:id', async(req, res)=> {
+      const id = req.params.id;
+      const post = await postCollection.findOne({_id: ObjectId(id)});
+      res.send(post);
+    })
 
-    
-     
+    app.post('/post', async(req, res)=> {
+      const post = req.body;
+      const data =  {
+        ...post, 
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }
+      const result = await postCollection.insertOne(data);
+      res.send(result);
+
+    })
+    app.delete("/post/:id", async (req, res) => {
+      const id = req.params.id;
+
+      const result = await postCollection.deleteOne({ _id: ObjectId(id) });
+      res.send(result);
+    });
+
+    app.patch('/post/:id', async(req, res)=> {
+      const id = req.params.id;
+      const data = req.body
+      const result = await postCollection.fineOne({_id: ObjectId(id)});
+      res.send(result);
+
+    })
+
   } finally {
   }
 };
